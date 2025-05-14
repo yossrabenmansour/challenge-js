d23bdeb7d6e9d2b49c98a4c964e481c4e01d1331
unknown <yossrabenmansour53@gmail.com>
2025-05-14 13:05:30 +0300

    done

--- app.js
+++ app.js
@@ -1,51 +1,77 @@
-// Challenge 3 ::
-const dScore1=96
-const dScore2=108
-const dScore3=89
+// // Challenge 3 ::
+// const dScore1=96
+// const dScore2=108
+// const dScore3=89
 
-const kScore1=88
-const kScore2=91
-const kScore3=110
-
-
-const dolphinScore=dScore1+dScore2+dScore3
-const koalaScore=kScore1+kScore2+kScore3
+// const kScore1=88
+// const kScore2=91
+// const kScore3=110
 
 
-console.log(dolphinScore)
-console.log(koalaScore)
+// const dolphinScore=dScore1+dScore2+dScore3
+// const koalaScore=kScore1+kScore2+kScore3
 
 
-const avgDolphin=dolphinScore/3
-const avgKoala=koalaScore/3
+// console.log(dolphinScore)
+// console.log(koalaScore)
 
 
-if(avgDolphin>avgKoala){
-    console.log("Dolphins win 🏆 ")
-}else if (avgKoala>avgDolphin){
-  console.log("Koalas win 🏆")
-}else{
-     console.log("Draw🤝")
-}
+// const avgDolphin=dolphinScore/3
+// const avgKoala=koalaScore/3
 
 
-// avec bonus ::
-if(avgDolphin>avgKoala && avgDolphin>=100){
-    console.log("Dolphins win 🏆 ")
-}else if (avgKoala>avgDolphin  && avgKoala >= 100){
-    console.log("Koalas win 🏆")
-}else if(avgDolphin ===avgKoala && avgDolphin >= 100 && avgKoala >= 100){
-    console.log("Draw 🤝")
-}else{
-    console.log("no team wins the trophy 🚫")
-}
+// if(avgDolphin>avgKoala){
+//     console.log("Dolphins win 🏆 ")
+// }else if (avgKoala>avgDolphin){
+//   console.log("Koalas win 🏆")
+// }else{
+//      console.log("Draw🤝")
+// }
+
+
+// // avec bonus ::
+// if(avgDolphin>avgKoala && avgDolphin>=100){
+//     console.log("Dolphins win 🏆 ")
+// }else if (avgKoala>avgDolphin  && avgKoala >= 100){
+//     console.log("Koalas win 🏆")
+// }else if(avgDolphin ===avgKoala && avgDolphin >= 100 && avgKoala >= 100){
+//     console.log("Draw 🤝")
+// }else{
+//     console.log("no team wins the trophy 🚫")
+// }
 
 // challenge 4
 
-const bill=430
+// const bill=430
 
-const tip= bill>=50 && bill<=300 ? bill*0.15: bill*0.20 
+// const tip= bill>=50 && bill<=300 ? bill*0.15: bill*0.20 
 
-const total=bill+tip
+// const total=bill+tip
 
-console.log(`the bill was ${bill},the tip was ${tip}, and the total value ${total}`)
+// console.log(`the bill was ${bill},the tip was ${tip}, and the total value ${total}`)
+
+
+///coding-chalange-func
+
+
+const calcAverage=(score1,score2,score3)=>(score1+score2+score3)/3
+
+const avgDolphin1=calcAverage(44,23,71)
+const avgKoala1=calcAverage(65,54,49)
+
+const avgDolphin2=calcAverage(85,54,41)
+const avgKoala2=calcAverage(23,34,27)
+
+
+function checkWinner(avgDolphin,avgKoala){
+    if (avgDolphin>=avgKoala*2){
+        console.log(`Dolphins win ${avgDolphin} vs ${avgKoala}`)
+    }else if (avgKoala>=avgDolphin*2){
+        console.log(`Koala win ${avgKoala} vs ${avgDolphin}`)
+    }else{
+        console.log("No team wins !! ")
+    }
+}
+
+checkWinner(avgDolphin1,avgKoala1)
+checkWinner(avgDolphin2,avgKoala2)
