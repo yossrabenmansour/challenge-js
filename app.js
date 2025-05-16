16dc3967113f23b4cc43c3e6eec06356acba4eba
unknown <yossrabenmansour53@gmail.com>
2025-05-16 12:51:06 +0300

    done

--- app.js
+++ app.js
@@ -54,24 +54,45 @@
 ///coding-chalange-func
 
 
-const calcAverage=(score1,score2,score3)=>(score1+score2+score3)/3
+// const calcAverage=(score1,score2,score3)=>(score1+score2+score3)/3
 
-const avgDolphin1=calcAverage(44,23,71)
-const avgKoala1=calcAverage(65,54,49)
+// const avgDolphin1=calcAverage(44,23,71)
+// const avgKoala1=calcAverage(65,54,49)
 
-const avgDolphin2=calcAverage(85,54,41)
-const avgKoala2=calcAverage(23,34,27)
+// const avgDolphin2=calcAverage(85,54,41)
+// const avgKoala2=calcAverage(23,34,27)
 
 
-function checkWinner(avgDolphin,avgKoala){
-    if (avgDolphin>=avgKoala*2){
-        console.log(`Dolphins win ${avgDolphin} vs ${avgKoala}`)
-    }else if (avgKoala>=avgDolphin*2){
-        console.log(`Koala win ${avgKoala} vs ${avgDolphin}`)
-    }else{
-        console.log("No team wins !! ")
-    }
+// function checkWinner(avgDolphin,avgKoala){
+//     if (avgDolphin>=avgKoala*2){
+//         console.log(`Dolphins win ${avgDolphin} vs ${avgKoala}`)
+//     }else if (avgKoala>=avgDolphin*2){
+//         console.log(`Koala win ${avgKoala} vs ${avgDolphin}`)
+//     }else{
+//         console.log("No team wins !! ")
+//     }
+// }
+
+// checkWinner(avgDolphin1,avgKoala1)
+// checkWinner(avgDolphin2,avgKoala2)
+
+
+/////funct
+
+//////1
+function calcTip(bill){
+  return   bill>=50 && bill<=300 ? bill*0.15: bill*0.20 
 }
 
-checkWinner(avgDolphin1,avgKoala1)
-checkWinner(avgDolphin2,avgKoala2)
+console.log(calcTip(100))
+ 
+const bills=[125,555,44]
+
+const tips=[calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])]
+
+const total=[bills[0]+tips[0], bills[1]+tips[1], bills[2]+bills[2]]
+
+console.log(bills)
+console.log(tips)
+console.log(total)
+
